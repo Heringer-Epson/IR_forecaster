e1f2f91e10f1df79373f08687268b937860455c3
Heringer-Epson <epson.heringer@gmail.com>
2019-11-19 01:16:03 -0500

    updates to IR webapp

--- webapp/app.py
+++ webapp/app.py
@@ -4,82 +4,28 @@ import dash
 from dash.dependencies import Input, Output
 import dash_html_components as html
 import dash_core_components as dcc
-import plotly.graph_objs as go
+from server import app
 
 from tabs import tab_IR
-
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src'))
-from master import Master
-
-
-M = Master().retrieve_data()
-
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-app.config.suppress_callback_exceptions = True
+from callbacks import callback_tab_1
 
 app.layout = html.Div([
-    html.H1('Dash Tabs component demo'),
-    dcc.Tabs(id='tabs-example', value='tab-1-example', children=[
-        dcc.Tab(label='Tab One', value='tab-1-example'),
+    html.H1('Analysis of Intrabank Rates'),
+    dcc.Tabs(id='tabs-main', value='tab-IR', children=[
+        dcc.Tab(label='IR', value='tab-IR'),
         dcc.Tab(label='Tab Two', value='tab-2-example'),
     ]),
-    html.Div(id='tabs-content-example')
+    html.Div(id='tabs-main-content')
 ])
 
-
-@app.callback(Output('tabs-content-example', 'children'),
-              [Input('tabs-example', 'value')])
+@app.callback(Output('tabs-main-content', 'children'),
+              [Input('tabs-main', 'value')])
 def render_content(tab):
-    if tab == 'tab-1-example':
+    if tab == 'tab-IR':
         return tab_IR.tab_IR_layout
         
     elif tab == 'tab-2-example':
-        return html.Div([
-            html.H3('Tab content 2'),
-            dcc.Graph(
-                id='graph-2-tabs',
-                figure={
-                    'data': [{
-                        'x': [1, 2, 3],
-                        'y': [5, 10, 6],
-                        'type': 'bar'
-                    }]
-                }
-            )
-        ])
-
-#Tab_IR_dropdown
-@app.callback(Output('page-1-content', 'figure'),
-              [Input('page-1-dropdown', 'value')])
-def page_1_dropdown(value):
-
-    M = Master(curr=value).retrieve_data()
-    dff = M['1m_1d']
-
-    traces = []
-    traces.append(go.Scatter(
-        x=dff['first_date'],
-        y=dff['ir_mean'],
-        text=dff['ir_mean'],
-        #x=[1, 2, 3],
-        #y=[6, 2, 3],
-        #text=['a', 'b', 'c'],
-        mode='lines',
-        opacity=0.7,
-        #marker={
-        #    'size': 15,
-        #    'line': {'width': 0.5, 'color': 'white'}
-        #},
-    ))
-
-    return {
-        'data': traces,
-        'layout': dict(
-            hovermode='closest',
-        )
-    }
+        return tab_IR.tab_IR_layout
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- webapp/tabs/tab_IR.py
+++ webapp/tabs/tab_IR.py
@@ -9,12 +9,48 @@ from master import Master
 
 tab_IR_layout = html.Div([
     html.H3('Intrabank Rates from LIBOR'),
-    dcc.Dropdown(
-        id='page-1-dropdown',
-        options=[{'label': i, 'value': i} for i in ['USD', 'CAD']],
-        value='USD'
-    ),
 
-    dcc.Graph(
-        id='page-1-content')
+    html.Div([
+        html.Div([
+            dcc.Dropdown(
+                id='tab-1-curr-dropdown',
+                options=[{'label': i, 'value': i} for i in ['USD', 'CAD']],
+                value='USD'
+            ),
+            dcc.Dropdown(
+                id='tab-1-tenor-dropdown',
+                options=[{'label': i, 'value': i} for i in ['1', '2', '3', '6', '12']],
+                value='1'
+            ),
+        ],
+        style={'width': '25%', 'display': 'inline-block'}),
+    ]),
+
+    html.Div([
+        html.Div([
+            dcc.RadioItems(
+                id='tab-1-transf-radio',
+                options=[{'label': i, 'value': i} for i in ['Increment', 'Log ratio']],
+                value='Increment',
+                labelStyle={'display': 'inline-block'}
+            ),
+
+        ],
+        style={'width': '25%', 'display': 'inline-block'}),
+    ]),
+
+
+    dcc.Graph(id='tab-1-graph'),
+ 
+    html.Div(
+        id='tab-1-slider',
+        style={'width': '80%', 'horizontal-align': 'middle'}
+        ),
+    #Add container showing the selected date range.
+    #improve slider placement.
+    #html.Div(id='tab-1-slider-container'),
+
+  
+ 
+    
 ])
