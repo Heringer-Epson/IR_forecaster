52f98077fcc82edc36de36cb2d681c3986f05f5d
Heringer-Epson <epson.heringer@gmail.com>
2019-11-20 23:58:24 -0500

    Improving dropdown layout

--- webapp/tabs/tab_IR.py
+++ webapp/tabs/tab_IR.py
@@ -1,30 +1,32 @@
-import sys
-import os
 from dash.dependencies import Input, Output
 import dash_html_components as html
 import dash_core_components as dcc
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', 'src'))
-from master import Master
-
 tab_IR_layout = html.Div([
-    html.H3('Intrabank Rates from LIBOR'),
+    
 
     html.Div([
-        html.Div([
-            dcc.Dropdown(
-                id='tab-IR-curr-dropdown',
-                options=[{'label': i, 'value': i} for i in ['USD', 'CAD']],
-                value='USD'
-            ),
-            dcc.Dropdown(
-                id='tab-IR-tenor-dropdown',
-                options=[{'label': i, 'value': i} for i in ['1', '2', '3', '6', '12']],
-                value='1'
-            ),
-        ],
-        style={'width': '25%', 'display': 'inline-block'}),
-    ]),
+        html.H6(
+            'Intrabank Rates from LIBOR',
+            style={'marginLeft': '3em', 'font-weight':'bold'}
+        ),
+        html.H6('Currency:', style={'marginLeft': '3.0em', }),
+        dcc.Dropdown(
+            id='tab-IR-curr-dropdown',
+            options=[{'label': i, 'value': i} for i in ['USD', 'CAD']],
+            value='USD',
+            style={'width': '100px', 'marginLeft': '.5em'},
+        ),       
+       
+        html.H6('Tenor:', style={'marginLeft': '3em'}),
+        dcc.Dropdown(
+            id='tab-IR-tenor-dropdown',
+            options=[{'label': i + ' month', 'value': i}
+                     for i in ['1', '2', '3', '6', '12']],
+            value='1',
+            style={'width': '150px', 'marginLeft': '.5em'},
+        ),
+        ], style={'display': 'flex', 'marginTop': '1.5em'}), 
 
     dcc.Graph(id='tab-IR-graph'),
  
