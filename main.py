a2c1d81c9377381c56f4fedc43f117f2ef70075e
Heringer-Epson <epson.heringer@gmail.com>
2019-12-01 18:11:42 -0500

    deployment test

--- main.py
+++ main.py
@@ -4,53 +4,21 @@ from dash.dependencies import Input, Output
 import dash_html_components as html
 import dash_core_components as dcc
 
-sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
-import utils
-from preprocess_data import Preproc_Data
+#sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
+#import utils
+#from preprocess_data import Preproc_Data
 import plotly.graph_objs as go
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-app.config.suppress_callback_exceptions = True
-
-
-
 
 from tabs import tab_about
-from tabs import tab_IR
-from tabs import tab_IRt
-from tabs import tab_hist
-from tabs import tab_term
-from tabs import tab_std
-from tabs import tab_corr
-from tabs import tab_pca
-#from tabs import tab_sim
-#from tabs import tab_pred
-
-#from callbacks import cb_IR
-from callbacks import cb_IRt
-from callbacks import cb_hist
-from callbacks import cb_term
-from callbacks import cb_std
-from callbacks import cb_corr
-from callbacks import cb_pca
-#from callbacks import cb_sim
-#from callbacks import cb_pred
 
 app.title = 'IR Forecaster'
 app.layout = html.Div([
     #html.H1('Analysis of Intrabank Rates'),
     dcc.Tabs(id='tabs-main', value='tab-about', children=[
         dcc.Tab(label='About', value='tab-about'),
-        dcc.Tab(label='Rates', value='tab-IR'),
-        dcc.Tab(label='Transf. Rates', value='tab-IR_t'),
-        dcc.Tab(label='Histogram', value='tab-hist'),
-        dcc.Tab(label='Term Structure', value='tab-term'),
-        dcc.Tab(label='Standard Dev.', value='tab-std'),
-        dcc.Tab(label='Correlation Matrix', value='tab-corr'),
-        dcc.Tab(label='PCA', value='tab-pca'),
-        #dcc.Tab(label='Simulate Rates', value='tab-sim'),
-        #dcc.Tab(label='Term Prediction', value='tab-pred'),
     ]),
     html.Div(id='tabs-main-content')
 ])
@@ -60,79 +28,7 @@ app.layout = html.Div([
 def render_content(tab):
     if tab == 'tab-about':
         return tab_about.tab_about_layout
-    elif tab == 'tab-IR':
-        return tab_IR.tab_IR_layout
-    elif tab == 'tab-IR_t':
-        return tab_IRt.tab_IRt_layout
-    elif tab == 'tab-hist':
-        return tab_hist.tab_hist_layout
-    elif tab == 'tab-term':
-        return tab_term.tab_term_layout
-    elif tab == 'tab-std':
-        return tab_std.tab_std_layout
-    elif tab == 'tab-corr':
-        return tab_corr.tab_corr_layout
-    elif tab == 'tab-pca':
-        return tab_pca.tab_pca_layout
-    #elif tab == 'tab-sim':
-    #    return tab_sim.tab_sim_layout
-    #elif tab == 'tab-pred':
-    #    return tab_pred.tab_pred_layout
-
-@app.callback(Output('tab-IR-graph', 'figure'),
-              [Input('tab-IR-curr-dropdown', 'value'),
-               Input('tab-IR-tenor-dropdown', 'value'),
-               Input('IR-year-slider', 'value')])
-def tab_IR_graph(curr, tenor, date_range):
-
-    t_min, t_max = utils.format_date(date_range)
-    M = Preproc_Data(curr=curr, t_ival=[t_min, t_max]).run()
-    df = M['{}m_1d'.format(str(tenor))]
-    
-    traces = []
-    traces.append(go.Scattergl(
-        x=df['date'],
-        y=df['ir'],
-        text=df['ir_transf'],
-        mode='lines',
-        opacity=.5,
-        line=dict(color='black', width=3.),
-        name='1 day',
-    ))
-
-    return {
-        'data': traces,
-        'layout': dict(
-            xaxis={'title': 'Date',},
-            yaxis={'title': 'IR',},
-            hovermode='closest',
-        )
-    }
-
-@app.callback(Output('tab-IR-slider', 'children'),
-              [Input('tab-IR-curr-dropdown', 'value'),
-               Input('tab-IR-tenor-dropdown', 'value')])
-def tab_IR_slider(curr, tenor):
-    t_min, t_max, t_list = utils.compute_t_range(
-      currtag=curr, tenor=[int(tenor)])
-    return html.Div(
-        dcc.RangeSlider(
-            id='IR-year-slider',
-            min=t_min,
-            max=t_max,
-            value=[2010, 2015],
-            marks={year: str(year) for year in t_list},
-            step=1./12.
-        )
-    )  
-
-@app.callback(Output('tab-IR-slider-container', 'children'),
-              [Input('IR-year-slider', 'value')])
-def tab_IR_slider_container(date_range):
-    t_min, t_max = utils.format_date(date_range)
-    return 'Date range is "{}" -- "{}"'.format(t_min, t_max)
-
-                                
+                       
 if __name__ == '__main__':
     #app.run_server(host='127.0.0.1', port=8080, debug=True)
     #app.run_server(host='0.0.0.0', port=8080, debug=True)
