4bd3da8d7c9232842ba2eba1977274a1941ceee9
Heringer-Epson <epson.heringer@gmail.com>
2019-12-01 17:50:12 -0500

    deployment test

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import sys, os
 import dash
 from dash.dependencies import Input, Output
 import dash_html_components as html
 import dash_core_components as dcc
+
+sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
+import utils
+from preprocess_data import Preproc_Data
+import plotly.graph_objs as go
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -21,7 +27,7 @@ from tabs import tab_pca
 #from tabs import tab_sim
 #from tabs import tab_pred
 
-from callbacks import cb_IR
+#from callbacks import cb_IR
 from callbacks import cb_IRt
 from callbacks import cb_hist
 from callbacks import cb_term
@@ -73,6 +79,58 @@ def render_content(tab):
     #elif tab == 'tab-pred':
     #    return tab_pred.tab_pred_layout
 
+@app.callback(Output('tab-IR-graph', 'figure'),
+              [Input('tab-IR-curr-dropdown', 'value'),
+               Input('tab-IR-tenor-dropdown', 'value'),
+               Input('IR-year-slider', 'value')])
+def tab_IR_graph(curr, tenor, date_range):
+
+    t_min, t_max = utils.format_date(date_range)
+    M = Preproc_Data(curr=curr, t_ival=[t_min, t_max]).run()
+    df = M['{}m_1d'.format(str(tenor))]
+    
+    traces = []
+    traces.append(go.Scattergl(
+        x=df['date'],
+        y=df['ir'],
+        text=df['ir_transf'],
+        mode='lines',
+        opacity=.5,
+        line=dict(color='black', width=3.),
+        name='1 day',
+    ))
+
+    return {
+        'data': traces,
+        'layout': dict(
+            xaxis={'title': 'Date',},
+            yaxis={'title': 'IR',},
+            hovermode='closest',
+        )
+    }
+
+@app.callback(Output('tab-IR-slider', 'children'),
+              [Input('tab-IR-curr-dropdown', 'value'),
+               Input('tab-IR-tenor-dropdown', 'value')])
+def tab_IR_slider(curr, tenor):
+    t_min, t_max, t_list = utils.compute_t_range(
+      currtag=curr, tenor=[int(tenor)])
+    return html.Div(
+        dcc.RangeSlider(
+            id='IR-year-slider',
+            min=t_min,
+            max=t_max,
+            value=[2010, 2015],
+            marks={year: str(year) for year in t_list},
+            step=1./12.
+        )
+    )  
+
+@app.callback(Output('tab-IR-slider-container', 'children'),
+              [Input('IR-year-slider', 'value')])
+def tab_IR_slider_container(date_range):
+    t_min, t_max = utils.format_date(date_range)
+    return 'Date range is "{}" -- "{}"'.format(t_min, t_max)
 
                                 
 if __name__ == '__main__':
