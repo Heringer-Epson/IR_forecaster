c32ac4508083a06285b24ffb8cc9c7b2c0eec10d
Heringer-Epson <epson.heringer@gmail.com>
2019-12-01 18:30:23 -0500

    Updating naming.

--- main.py
+++ main.py
@@ -1,25 +1,44 @@
-import sys, os
 import dash
 from dash.dependencies import Input, Output
 import dash_html_components as html
 import dash_core_components as dcc
-import base64
+from server import app
 
-#sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
-#import utils
-#from preprocess_data import Preproc_Data
-import plotly.graph_objs as go
+from tabs import tab_about
+from tabs import tab_IR
+from tabs import tab_IRt
+from tabs import tab_hist
+from tabs import tab_term
+from tabs import tab_std
+from tabs import tab_corr
+from tabs import tab_pca
+from tabs import tab_sim
+from tabs import tab_pred
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-#from tabs import tab_about
+from callbacks import cb_IR
+from callbacks import cb_IRt
+from callbacks import cb_hist
+from callbacks import cb_term
+from callbacks import cb_std
+from callbacks import cb_corr
+from callbacks import cb_pca
+from callbacks import cb_sim
+from callbacks import cb_pred
 
 app.title = 'IR Forecaster'
 app.layout = html.Div([
     #html.H1('Analysis of Intrabank Rates'),
     dcc.Tabs(id='tabs-main', value='tab-about', children=[
         dcc.Tab(label='About', value='tab-about'),
+        dcc.Tab(label='Rates', value='tab-IR'),
+        dcc.Tab(label='Transf. Rates', value='tab-IR_t'),
+        dcc.Tab(label='Histogram', value='tab-hist'),
+        dcc.Tab(label='Term Structure', value='tab-term'),
+        dcc.Tab(label='Standard Dev.', value='tab-std'),
+        dcc.Tab(label='Correlation Matrix', value='tab-corr'),
+        dcc.Tab(label='PCA', value='tab-pca'),
+        dcc.Tab(label='Simulate Rates', value='tab-sim'),
+        dcc.Tab(label='Term Prediction', value='tab-pred'),
     ]),
     html.Div(id='tabs-main-content')
 ])
@@ -28,114 +47,26 @@ app.layout = html.Div([
               [Input('tabs-main', 'value')])
 def render_content(tab):
     if tab == 'tab-about':
-        return tab_about_layout
-
-term_png = './images/term.png'
-term_base64 = base64.b64encode(open(term_png, 'rb').read()).decode('ascii')
-
-paths_png = './images/paths.png'
-paths_base64 = base64.b64encode(open(paths_png, 'rb').read()).decode('ascii')
-
-tab_about_layout = html.Div([
-    
-    #Header and authorship.
-    html.Div([
-        html.H3(
-            'Simulating future intrabank rates',
-            style={'font-weight':'bold'}
-        ),
-        html.H3(
-            'v(1.0.0)',
-            style={'marginLeft': '0.75em'}
-        ),    
-    ], style={'display': 'flex', 'marginLeft': '3em', 'marginTop': '1.5em'}),
-    
-    html.H6(
-        'by Epson Heringer',
-        style={'marginLeft': '4em', 'marginTop': '-1em', 'font-style':'italic'}
-    ),
-
-    #Body.
-    #E.g. https://community.plot.ly/t/two-graphs-side-by-side/5312
-    html.Div([
-
-        html.Div([
-
-            #Left panel.
-            html.Div([
-                dcc.Markdown(
-                    """
-                    ###### OVERVIEW
-                    This web app provides interactive plotting and analytical
-                    functions to facilitate the analysis
-                    
-                    of intrabank rates. The user may choose to work with
-                    transformed rates (rate differences or
-                     
-                    log of rate ratios), over a daily or monthly period. Such
-                    mappings typically improve the model accuracy.
-                    
-                    &nbsp;&nbsp;&nbsp;&nbsp;
-                    
-                    Future rates are estimated using a Monte Carlo simulation,
-                    where random rates (or mapped rates)
-                    
-                    are drawn from the distributions that best fits each tenor.
-                    The user may also choose which finance model
-                    
-                    to use to compute forward rates, noting that the correlation 
-                    between tenors is taken into account.
-                    
-                    Finally, the user may enable the principal component analysis
-                    (PCA) option to reduce the dimensionality of the data.
-
-                    &nbsp;&nbsp;&nbsp;&nbsp;
-                    
-                    ###### DATA
-                    The rates used here are from the London Interbank Offered
-                    Rate (LIBOR). 
-                    
-                    Source: The Federal Bank of Saint-Louis ([FRED](https://fred.stlouisfed.org)).
-
-                    &nbsp;&nbsp;&nbsp;&nbsp;
-
-                    ###### TECHNICAL DETAILS
-                    
-                    This web app is deployed on the Google Cloud Platform
-                    and is written in python 3.7, using Dash.
-                    
-                    Finance models are implemented via an R wrapper to call
-                    the [fitsde](https://cran.r-project.org/web/packages/Sim.DiffProc/vignettes/fitsde.html)
-                    routine from the  [Sim.DiffProc](https://cran.r-project.org/web/packages/Sim.DiffProc/index.html) package.
-                    
-                    Github source code: https://github.com/Heringer-Epson/IR_forecaster
-                    """,
-                    style={'marginLeft': '3em'},
-                ),      
-                
-            ], className="six columns"),
-
-            #Right panel.
-
-            html.Div([
-                html.Img(
-                  src='data:image/png;base64,{}'.format(term_base64),
-                  style={'width':'700px', 'height':'400px', 'margin':'auto'}),
-                html.Img(
-                    src='data:image/png;base64,{}'.format(paths_base64),
-                    style={'width':'700px', 'height':'400px', 'marginTop':'-5em'}),
-                dcc.Markdown(
-                    """
-                    Disclaimer: The author does not take responsability in
-                    profits or losses occuring from the usage of this app. 
-                    """
-                )
-            ], style={'marginTop': '-10em'}, className="six columns"),
-        ], className="row")
-    ])
-])   
-                       
+        return tab_about.tab_about_layout
+    elif tab == 'tab-IR':
+        return tab_IR.tab_IR_layout
+    elif tab == 'tab-IR_t':
+        return tab_IRt.tab_IRt_layout
+    elif tab == 'tab-hist':
+        return tab_hist.tab_hist_layout
+    elif tab == 'tab-term':
+        return tab_term.tab_term_layout
+    elif tab == 'tab-std':
+        return tab_std.tab_std_layout
+    elif tab == 'tab-corr':
+        return tab_corr.tab_corr_layout
+    elif tab == 'tab-pca':
+        return tab_pca.tab_pca_layout
+    elif tab == 'tab-sim':
+        return tab_sim.tab_sim_layout
+    elif tab == 'tab-pred':
+        return tab_pred.tab_pred_layout
+                                
 if __name__ == '__main__':
     #app.run_server(host='127.0.0.1', port=8080, debug=True)
-    #app.run_server(host='0.0.0.0', port=8080, debug=True)
-    app.run_server(debug=True)
+    app.run_server(host='0.0.0.0', port=8080, debug=True)
