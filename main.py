2a345bbd2294d32f921fca13ae5e753b98343139
Heringer-Epson <epson.heringer@gmail.com>
2019-12-02 14:49:31 -0500

    Test-Deploying with about tab only.

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import dash
 from dash.dependencies import Input, Output
 import dash_html_components as html
 import dash_core_components as dcc
-from server import app
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+dash_app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+dash_app.config.suppress_callback_exceptions = True
+app = dash_app.server
 
 #deploy#
 
@@ -17,39 +21,30 @@ from tabs import tab_pca
 from tabs import tab_sim
 from tabs import tab_pred
 
-from callbacks import cb_IR
-from callbacks import cb_IRt
-from callbacks import cb_hist
-from callbacks import cb_term
-from callbacks import cb_std
-from callbacks import cb_corr
-from callbacks import cb_pca
-from callbacks import cb_sim
-from callbacks import cb_pred
-
-app.title = 'IR Forecaster'
-app.layout = html.Div([
+dash_app.title = 'IR Forecaster'
+dash_app.layout = html.Div([
     #html.H1('Analysis of Intrabank Rates'),
     dcc.Tabs(id='tabs-main', value='tab-about', children=[
         dcc.Tab(label='About', value='tab-about'),
-        dcc.Tab(label='Rates', value='tab-IR'),
-        dcc.Tab(label='Transf. Rates', value='tab-IR_t'),
-        dcc.Tab(label='Histogram', value='tab-hist'),
-        dcc.Tab(label='Term Structure', value='tab-term'),
-        dcc.Tab(label='Standard Dev.', value='tab-std'),
-        dcc.Tab(label='Correlation Matrix', value='tab-corr'),
-        dcc.Tab(label='PCA', value='tab-pca'),
-        dcc.Tab(label='Simulate Rates', value='tab-sim'),
-        dcc.Tab(label='Term Prediction', value='tab-pred'),
+        #dcc.Tab(label='Rates', value='tab-IR'),
+        #dcc.Tab(label='Transf. Rates', value='tab-IR_t'),
+        #dcc.Tab(label='Histogram', value='tab-hist'),
+        #dcc.Tab(label='Term Structure', value='tab-term'),
+        #dcc.Tab(label='Standard Dev.', value='tab-std'),
+        #dcc.Tab(label='Correlation Matrix', value='tab-corr'),
+        #dcc.Tab(label='PCA', value='tab-pca'),
+        #dcc.Tab(label='Simulate Rates', value='tab-sim'),
+        #dcc.Tab(label='Term Prediction', value='tab-pred'),
     ]),
     html.Div(id='tabs-main-content')
 ])
 
-@app.callback(Output('tabs-main-content', 'children'),
+@dash_app.callback(Output('tabs-main-content', 'children'),
               [Input('tabs-main', 'value')])
 def render_content(tab):
     if tab == 'tab-about':
         return tab_about.tab_about_layout
+    '''
     elif tab == 'tab-IR':
         return tab_IR.tab_IR_layout
     elif tab == 'tab-IR_t':
@@ -68,7 +63,9 @@ def render_content(tab):
         return tab_sim.tab_sim_layout
     elif tab == 'tab-pred':
         return tab_pred.tab_pred_layout
+    '''
                                 
 if __name__ == '__main__':
-    #app.run_server(host='127.0.0.1', port=8080, debug=True)
-    app.run_server(host='0.0.0.0', port=8080, debug=True)
+    #dash_app.run_server(host='127.0.0.1', port=8080, debug=True)
+    #dash_app.run_server(host='0.0.0.0', port=8080, debug=True)
+    dash_app.run_server(debug=True)
