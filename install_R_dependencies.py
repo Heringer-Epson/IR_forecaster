950437af11ddfedd3252914e91366905bb37f19a
Heringer-Epson <epson.heringer@gmail.com>
2019-12-04 10:30:22 -0500

    Stable version for deployment. Updated set_env.txt.

--- install_R_dependencies.py
+++
@@ -1,5 +0,0 @@
-import rpy2
-from rpy2.robjects.packages import importr
-utils = importr('utils')
-utils.install_packages('Sim.DiffProc', repos="http://cran.us.r-project.org")
-importr('Sim.DiffProc')
