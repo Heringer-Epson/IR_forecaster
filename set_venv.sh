27cc4a1e9572222d86e70dac495479e37f785134
Heringer-Epson <epson.heringer@gmail.com>
2019-12-04 00:26:55 -0500

    Deployed successfully. Set debug=False for deployment.

--- set_venv.sh
+++ set_venv.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-mkdir /home/R_packages
+sudo mkdir /home/R_packages
 chmod a+rwx -R /home/R_packages
 
 python3 -m venv .venvs/ir_env
 source .venvs/ir_env/bin/activate
 
-apt-get update -y
-apt-get install -qy python3-pip
-apt-get install -qy  r-base
+sudo apt-get update -y
+sudo apt-get install -qy python3-pip
+sudo apt-get install -qy  r-base
 cd IR_forecaster
 pip3 install -r requirements.txt
-Rscript -e 'install.packages("Sim.DiffProc", lib="/home/R_packages", repos="http://cran.us.r-project.org")'
+sudo Rscript -e 'install.packages("Sim.DiffProc", lib="/home/R_packages", repos="http://cran.us.r-project.org")'
 python3 main.py
